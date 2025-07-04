1f8a1065bdcdd22f78bc28ac59df5c74ea2fb1b7
Emmanuvel Sajan <153431487+NotSKDev@users.noreply.github.com>
2025-07-04 11:24:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-1const axios = require("axios");
-2
-3class X0xA {
-4  constructor() {
-5    this.x1 = "https://notsk-news-rssfeed.onrender.com/news-data";
-6    this.x2 = {
-7      Authorization: "404",
-8      "User-Agent": "NotBrowser-Fetcher",
-9    };
-10  }
-11
-12  async getLatest() {
-13    const r = await axios.get(this.x1, { headers: this.x2 });
-14    const d = r.data;
-15    return {
-16      title: d.title,
-17      link: d.link,
-18      image: d.image,
-19      channel_url: d.channel_url,
-20      channel_logo: d.channel_logo,
-21      channel_name: d.channel_name,
-22    };
-23  }
-24}
-25
-26module.exports = function () {
-27  if (!(this instanceof module.exports)) {
-28    console.error(
-29      "⛔ Use as constructor: const feed = new require('notsk-twentyfournews-feed')()"
-30    );
-31    process.exit(1);
-32  }
-33
-34  return new X0xA();
-35};
+const axios = require("axios");
+
+class X0xA {
+  constructor() {
+    this.x1 = "https://notsk-news-rssfeed.onrender.com/news-data";
+    this.x2 = {
+      Authorization: "404",
+      "User-Agent": "NotBrowser-Fetcher",
+    };
+  }
+
+  async getLatest() {
+    const r = await axios.get(this.x1, { headers: this.x2 });
+    const d = r.data;
+    return {
+      title: d.title,
+      link: d.link,
+      image: d.image,
+      channel_url: d.channel_url,
+      channel_logo: d.channel_logo,
+      channel_name: d.channel_name,
+    };
+  }
+}
+
+module.exports = function () {
+  if (!(this instanceof module.exports)) {
+    console.error(
+      "⛔ Use as constructor: const feed = new require('notsk-twentyfournews-feed')()"
+    );
+    process.exit(1);
+  }
+
+  return new X0xA();
+};
