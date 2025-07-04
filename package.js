df28b9ced47e441b796f11320029add5e037bcfb
Emmanuvel Sajan <153431487+NotSKDev@users.noreply.github.com>
2025-07-04 07:55:09 +0530

    Create package.js

---
+++ package.js
@@ -0,0 +1,24 @@
+1{
+2  "name": "notsk-news-feeder",
+3  "version": "1.0.0",
+4  "description": "Fetch latest Malayalam news from TwentyFourNews.com NPM package.",
+5  "main": "index.js",
+6  "scripts": {
+7    "test": "echo \"Error: no test specified\" && exit 1"
+8  },
+9  "repository": {
+10    "type": "git",
+11    "url": "git+https://github.com/NotSKDev/notsk-news-feeder.git"
+12  },
+13  "keywords": [
+14    "axios",
+15    "news",
+16    "RSSfeeder"
+17  ],
+18  "author": "NotSK",
+19  "license": "ISC",
+20  "bugs": {
+21    "url": "https://github.com/NotSKDev/notsk-news-feeder/issues"
+22  },
+23  "homepage": "https://github.com/NotSKDev/notsk-news-feeder#readme"
+24}
